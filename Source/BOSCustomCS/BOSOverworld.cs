ef18dc9d58bbbf7b584660764df1294cdcb3cbec
sky_is_you <sky0is0you@gmail.com>
2026-08-29 17:47:57 -0400

    update packages

--- Source/BOSCustomCS/BOSOverworld.cs
+++ Source/BOSCustomCS/BOSOverworld.cs
@@ -4,6 +4,14 @@ namespace Celeste.Mod.BetterOverworldSwitcher.BOSCustomCS;
 
 public class BOSOverworld : Scene
 {
+    // what we wanna do:
+    // - copy vanilla overworld
+    // - except add system to cleanly hand full control over to mods' overworlds TODO: should we give full control?
+    // - '- other option: use config files + a separate editor app (ewie)
+    // - config files for like better detail on how to switch over might be needed
+    // - first hand over control to any other specified default or just go with our self
+    // - !!! mod overworlds will have to cooperate for us to get control back cleanly
+    // - fallback to us first; we can get diagnostics. fallback to vanilla if we fail !
     private MountainRenderer Mountain;
     private HiresSnow Snow;
     public BOSOverworld(OverworldLoader loader)
